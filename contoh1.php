ad4d230927dc074369c400538d2dc47e89fd905e
alvania20 <fawwasalghifari20@gmail.com>
2022-03-17 15:51:02 +0700

    menambahkan file baru contoh1.php

---
+++ contoh1.php
@@ -0,0 +1,11 @@
+<?php
+class contoh1 extends CI_Controller
+{
+    public function index()
+    {
+        echo "<h1>Perkenalan<h1>";
+        echo "Nama Saya Fawwas Alghifari
+            Saya Tinggal di daerah Ciputat
+            olahraga yg saya sukai adalah renang";
+    }
+}
